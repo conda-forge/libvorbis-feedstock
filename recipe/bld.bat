bce6901db154522ea86c8a675d939c1c6554eb23
martinRenou <martin.renou@gmail.com>
2020-03-23 13:13:20 +0100

    Build as a shared library
    Signed-off-by: martinRenou <martin.renou@gmail.com>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,6 +3,7 @@ setlocal EnableDelayedExpansion
 cmake -G "NMake Makefiles" ^
   -D CMAKE_BUILD_TYPE=Release ^
   -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+  -D BUILD_SHARED_LIBS=ON ^
   %SRC_DIR%
 if errorlevel 1 exit 1
 
--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,7 @@
 cmake -DCMAKE_BUILD_TYPE=Release     \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=lib     \
+      -DBUILD_SHARED_LIBS=ON         \
       $SRC_DIR
 
 make install
